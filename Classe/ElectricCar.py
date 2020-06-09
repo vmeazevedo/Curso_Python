1866e777d9afaa0cc8bcef2d235e16d2e3c776e8
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-09 13:38:55 -0300

    Update ElectricCar.py

--- Classe/ElectricCar.py
+++ Classe/ElectricCar.py
@@ -29,7 +29,7 @@ class Car():
         '''Soma a quantidade especifica ao valor de leitura do odômetro.'''
         self.odometer_reading += miles
 
-
+#FILHA
 class Battery():
     '''Uma tentativa simples de modelar uma bateria para um carro elétrico.'''
 
@@ -68,4 +68,24 @@ my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
 
 
+'''
+9.6 - Sorveteria: Uma sorveteria é um tipo específico de restaurante. Escreva uma classe chamada IceCreamStand
+ que herde da classe Restaurant escrita no ex 9.1. Qualquer versão da classe funcionará, basta escolher aquela 
+ de que você mais gosta. Adicione um atributo chamado flavors que armazene uma lista de sabores de sorvete.
+ Escreva um método para mostrar esses sabores. Crie uma instância de IceCreamStand e chame esse método.'''
+ print('\n9.6 - Sorveteria')
+ class Restaurant():
 
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        print('O restaurante ' + self.restaurant_name.title() + ' tem uma cozinha ' + self.cuisine_type + '.')
+
+    def open_restaurant(self):
+        print('O restaurante ' + self.restaurant_name.title() + ' está aberto!')
+
+restaunt = Restaurant('Ragazzo', 'organizada')
+restaunt.describe_restaurant()
+restaunt.open_restaurant()
