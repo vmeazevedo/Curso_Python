c391203e607cd31aee8848778bc55f8277f28675
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-09 14:13:35 -0300

    Update ElectricCar.py

--- Classe/ElectricCar.py
+++ Classe/ElectricCar.py
@@ -100,3 +100,42 @@ restaunt.open_restaurant()
 
 sorvete = IceCreamStand('Ragazzo', 'organizada')                        #Instância de IceCreamStand criada
 sorvete.describe_flavors()                                              #Chamando o método com as informações de sabores.
+
+
+'''
+9.7 - Admin: Um administrador é um tipo especial de usuário. Escreva uma classe chamada Admin
+ que herde da classe User escrita no ex 9.3. Adicione um atributo privileges que armazene uma lista de strings como 
+ 'can add post', 'can delete post', 'can ban user', e assim por diante. Escreva uma método chamado show_privileges()
+ que liste o conjunto de privilégios de um adiministrador. Crie uma instância de admin e chame seu método.'''
+print('\n9.7- Admin')
+class User():
+    def __init__(self, first_name, last_name, age, city='santo andre'):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+    
+    def describe_user(self):
+        print('\nNome: ' + self.first_name.title() + ' ' + self.last_name.title() + ', Cidade: ' + self.city.title() + ', Idade: ' + str(self.age) + '.')
+
+    def greet_user(self):
+        full_name = self.first_name + ' ' + self.last_name
+        print('Olá ' + full_name.title() + ', seja bem vindo ao Itaú!' )
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, age, city='santo andre'):
+        super().__init__(first_name, last_name, age, city=city)
+        self.privileges = ['can add post', 'can delete post', 'can ban user']
+
+    def show_privileges(self):
+        print('Os privilégios desse usuário são:')
+        for n in self.privileges:
+            print(n.capitalize() + '.')   
+
+info = User('vinicius', 'azevedo', 28, 'mauá')
+info.describe_user()
+info.greet_user()
+
+priv = Admin('vinicius', 'azevedo', 28, 'mauá')
+priv.show_privileges()
