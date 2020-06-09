4267f83c89e85f08623ad51d81ef9312eee8d92e
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-09 13:35:03 -0300

    Update ElectricCar.py
    update

--- Classe/ElectricCar.py
+++ Classe/ElectricCar.py
@@ -29,12 +29,43 @@ class Car():
         '''Soma a quantidade especifica ao valor de leitura do odômetro.'''
         self.odometer_reading += miles
 
+
+class Battery():
+    '''Uma tentativa simples de modelar uma bateria para um carro elétrico.'''
+
+    def __init__(self, battery_size=70):
+        '''Inicializa os atributos da bateria.'''
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''Exibe uma frase que descreve a capacidade da bateria.'''
+        print('This car has a ' + str(self.battery_size) + '-kWh battery.') 
+
+    def get_range(self):
+        '''Exibe uma frase sobre a distância que o carro é capaz de percorrer com essa bateria.'''
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        
+        message = 'This car can go approximately ' + str(range) + ' miles on full charge.'
+        print(message)
+
 class ElectricCar(Car):
     '''Representa aspectos de um carro específicos de veículos elétricos.'''
 
     def __init__(self, make, model, year):
-        '''Inicializa os atributos da classe-pai.'''
+        '''
+        Inicializa os atributos da classe-pai.
+        Em seguida, inicializa os atributos específicos de um carro elétrico.
+        '''
         super().__init__(make, model, year)
+        self.battery = Battery()
 
 my_tesla = ElectricCar('tesla', 'model s', '2016')
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
+
+
