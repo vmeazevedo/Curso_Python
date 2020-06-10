a7def15fd7bb9923c488f3022fe56d4010a8d4b1
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-10 12:43:29 -0300

    Update ElectricCar.py
    upgrade de bateria

--- Classe/ElectricCar.py
+++ Classe/ElectricCar.py
@@ -1,71 +1,71 @@
-class Car():
-    '''Uma tentativa simples de representar um carro.'''
+# class Car():
+#     '''Uma tentativa simples de representar um carro.'''
 
-    def __init__(self, make, model, year):
-        '''Inicializa os atributos que descrevem um carro.'''
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+#     def __init__(self, make, model, year):
+#         '''Inicializa os atributos que descrevem um carro.'''
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
 
-    def get_descriptive_name(self):
-        '''Devolve um nome descritivo, formatado de modo elegante.'''
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         '''Devolve um nome descritivo, formatado de modo elegante.'''
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
     
-    def read_odometer(self):
-        '''Exibe uma frase que mostra a milhagem do carro.'''
-        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+#     def read_odometer(self):
+#         '''Exibe uma frase que mostra a milhagem do carro.'''
+#         print('This car has ' + str(self.odometer_reading) + ' miles on it.')
 
-    def update_odometer(self, mileage):
-        '''Define o valor de leitura do odômetro com o valor especificado.
-        Rejeita a alteração se for tentativa de definir um valor menor para o odômetro.'''
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print('You can not go back a odometer!')
+#     def update_odometer(self, mileage):
+#         '''Define o valor de leitura do odômetro com o valor especificado.
+#         Rejeita a alteração se for tentativa de definir um valor menor para o odômetro.'''
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print('You can not go back a odometer!')
 
-    def increment_odometer(self, miles):
-        '''Soma a quantidade especifica ao valor de leitura do odômetro.'''
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         '''Soma a quantidade especifica ao valor de leitura do odômetro.'''
+#         self.odometer_reading += miles
 
-#FILHA
-class Battery():
-    '''Uma tentativa simples de modelar uma bateria para um carro elétrico.'''
+# #FILHA
+# class Battery():
+#     '''Uma tentativa simples de modelar uma bateria para um carro elétrico.'''
 
-    def __init__(self, battery_size=70):
-        '''Inicializa os atributos da bateria.'''
-        self.battery_size = battery_size
+#     def __init__(self, battery_size=70):
+#         '''Inicializa os atributos da bateria.'''
+#         self.battery_size = battery_size
 
-    def describe_battery(self):
-        '''Exibe uma frase que descreve a capacidade da bateria.'''
-        print('This car has a ' + str(self.battery_size) + '-kWh battery.') 
+#     def describe_battery(self):
+#         '''Exibe uma frase que descreve a capacidade da bateria.'''
+#         print('This car has a ' + str(self.battery_size) + '-kWh battery.') 
 
-    def get_range(self):
-        '''Exibe uma frase sobre a distância que o carro é capaz de percorrer com essa bateria.'''
-        if self.battery_size == 70:
-            range = 240
-        elif self.battery_size == 85:
-            range = 270
+#     def get_range(self):
+#         '''Exibe uma frase sobre a distância que o carro é capaz de percorrer com essa bateria.'''
+#         if self.battery_size == 70:
+#             range = 240
+#         elif self.battery_size == 85:
+#             range = 270
         
-        message = 'This car can go approximately ' + str(range) + ' miles on full charge.'
-        print(message)
+#         message = 'This car can go approximately ' + str(range) + ' miles on full charge.'
+#         print(message)
 
-class ElectricCar(Car):
-    '''Representa aspectos de um carro específicos de veículos elétricos.'''
+# class ElectricCar(Car):
+#     '''Representa aspectos de um carro específicos de veículos elétricos.'''
 
-    def __init__(self, make, model, year):
-        '''
-        Inicializa os atributos da classe-pai.
-        Em seguida, inicializa os atributos específicos de um carro elétrico.
-        '''
-        super().__init__(make, model, year)
-        self.battery = Battery()
+#     def __init__(self, make, model, year):
+#         '''
+#         Inicializa os atributos da classe-pai.
+#         Em seguida, inicializa os atributos específicos de um carro elétrico.
+#         '''
+#         super().__init__(make, model, year)
+#         self.battery = Battery()
 
-my_tesla = ElectricCar('tesla', 'model s', '2016')
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla = ElectricCar('tesla', 'model s', '2016')
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
 
 
 '''
@@ -181,6 +181,80 @@ priv = Admin('vinicius', 'azevedo', 28, 'mauá')
 priv.privilegios.show_privileges()
 
 
-
 '''
-9.9 - Upgrade de bateria:'''
+9.9 - Upgrade de bateria: Use a última versão de electric_car.py desta seção. Acrescente um método chamado
+upgrade_battery() na classe Battery. Esse método deve verificar a capacidade da bateria e defini-la com 85
+se o valor for diferente. Crie um carro elétrico com uma capacidade de bateria default, chame get_range()
+uma vez e, em seguida, chame get_range() uma segunda vez após fazer um upgrade de bateria. Você deverá ver
+um aumento na distância que o carro é capaz de percorrer.'''
+print('\n9.9 - Upgrade de bateria')
+class Car():
+    '''Uma tentativa simples de representar um carro.'''
+    def __init__(self, make, model, year):
+        '''Inicializa os atributos que descrevem um carro.'''
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        '''Devolve um nome descritivo, formatado de modo elegante.'''
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        '''Exibe uma frase que mostra a milhagem do carro.'''
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+    def update_odometer(self, mileage):
+        '''Define o valor de leitura do odômetro com o valor especificado.
+        Rejeita a alteração se for tentativa de definir um valor menor para o odômetro.'''
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print('You can not go back a odometer!')
+    def increment_odometer(self, miles):
+        '''Soma a quantidade especifica ao valor de leitura do odômetro.'''
+        self.odometer_reading += miles
+
+#FILHA
+class Battery():
+    '''Uma tentativa simples de modelar uma bateria para um carro elétrico.'''
+
+    def __init__(self, battery_size=70):
+        '''Inicializa os atributos da bateria.'''
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''Exibe uma frase que descreve a capacidade da bateria.'''
+        print('This car has a ' + str(self.battery_size) + '-kWh battery.') 
+
+    def get_range(self):
+        '''Exibe uma frase sobre a distância que o carro é capaz de percorrer com essa bateria.'''
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        
+        message = 'This car can go approximately ' + str(range) + ' miles on full charge.'
+        print(message)
+
+    def upgrade_battery(self):
+        if self.battery_size < 85:
+            self.battery_size = 85
+
+
+class ElectricCar(Car):
+    '''Representa aspectos de um carro específicos de veículos elétricos.'''
+
+    def __init__(self, make, model, year):
+        '''
+        Inicializa os atributos da classe-pai.
+        Em seguida, inicializa os atributos específicos de um carro elétrico.
+        '''
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', '2016')
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
