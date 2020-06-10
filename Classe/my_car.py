df36ca2be708b44baf2bbba0302dce3047a14d83
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-10 13:33:13 -0300

    Update my_car.py
    importando em diferentes metodos

--- Classe/my_car.py
+++ Classe/my_car.py
@@ -1,15 +1,18 @@
+#Importando várias classes de um módulo
 from car import Car, ElectricCar
 
-my_new_car = Car('audi', 'a4', '2016')
-print(my_new_car.get_descriptive_name())
-
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+my_beetle = Car('volkswagem', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+my_tesla = ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
 
 
 print()
-my_beetle = Car('volkswagem', 'beetle', 2016)
-print(my_beetle.get_descriptive_name())
+#Importando um módulo completo
+import car 
 
-my_tesla = ElectricCar('tesla', 'roadster', 2016)
+my_beetle = car.Car('volkswagem', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+my_tesla = car.ElectricCar('tesla', 'roadster', 2016)
 print(my_tesla.get_descriptive_name())
+
