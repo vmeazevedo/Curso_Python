0af0be68846338b4d2c20e2aa52e2aa980d27118
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-10 13:44:08 -0300

    Create exer.py
    pratica

---
+++ Classe/exer.py
@@ -0,0 +1,4 @@
+'''
+9.10 - Importando Restaurant: Usando sua classe Restaurant mais recente armazene-a em um módulo. Crie um arquivo
+separado que importe Restaurant. Crie uma instância de Restaurant e chame um de seus métodos para msotrar que a instrução
+import funciona de forma apropriada.'''
