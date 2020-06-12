dc0e3a8b70f93a190f4f230d85920e9b9ad6d4f5
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-12 13:40:34 -0300

    Update pi_strings.py

--- Arquivo_Exceções/pi_strings.py
+++ Arquivo_Exceções/pi_strings.py
@@ -9,3 +9,30 @@ for line in lines:
 
 print(pi_string)
 print(len(pi_string))
+
+#Arquivos grandes: um milhão de dígitos
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_million_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string[:52] + '...')
+print(len(pi_string))
+
+#Seu aniversário está contido em pi?
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_million_digits.txt'
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input('Enter your birthday, in the form mmddyy: ')
+if birthday in pi_string:
+    print('Your birthday appears in the firs million digits of pi!')
+else:
+    print('Your birthday does not appear in the first million digits of pi.')
