073c173bace898a3a9914eed059e9d836e243444
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-12 14:02:03 -0300

    Update exer.py
    update

--- Arquivo_Exceções/exer.py
+++ Arquivo_Exceções/exer.py
@@ -6,4 +6,24 @@ Escreva um programa que leia o arquivo e mostre o que você escreveu, três veze
 lendo o arquivo todo, uma vez percorrendo o objeto arquivo com um laço, e outra armazenando as linhas em uma lista
 e então trabalhando com ela fora do bloco with.
 '''
+#Exiba o conteúdo uma vez lendo o arquivo todo
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\learning_python.txt'     #Identificamos o local do arquivo
+with open(filename) as file_object:                                                                             #Abrimos o arquivo como objeto
+    conteudo = file_object.read()                                                                               #Acessamos a variavel com o metodo read.
+    print(conteudo)                                                                                             #Printamos o conteudo do arquivo    
+    print()
 
+#Uma vez percorrendo o objeto arquivo com um laço
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\learning_python.txt'     #Identificamos o local do arquivo
+with open(filename) as file_object:                                                                             #Abrimos o arquivo como objeto                 
+    for line in file_object:                                                                                    #Acessamos cada linha do conteudo do arquivo com um laço
+        print(line.rstrip())                                                                                    #Printamos o conteúdo formatado
+print()
+
+#E outra armazenando as linhas em uma lista e então trabalhando com ela fora do bloco with
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\learning_python.txt'     #Identificamos o local do arquivo     
+with open(filename) as file_object:                                                                             #Abrimos o arquivo como objeto
+    lines = file_object.readlines()                                                                             #Acessamos a variável com o metodo readlines
+
+for n in lines:                                                                                                 #Para cada item dentro da variável            
+    print(n.rstrip())                                                                                           #Printamos o conteúdo formatado
