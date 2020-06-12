621e124bf8349bd817b51e236ccd8a6811d4698b
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-12 12:26:09 -0300

    Update file_reader.py
    update

--- Arquivo_Exceções/file_reader.py
+++ Arquivo_Exceções/file_reader.py
@@ -1,3 +1,11 @@
-with open('pi_digits.txt') as file_object:
+import os
+
+#Importando um arquivo utilizando 'os'
+with open(os.path.join(os.path.dirname(__file__), 'pi_digits.txt')) as file_object:       
     contents = file_object.read()
-    print(contents)
+    print(contents.rstrip())
+
+#Importando um arquivo por diretorio absoluto
+# with open('C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_digits.txt') as file_object:
+#     contents = file_object.read()
+#     print(contents)
