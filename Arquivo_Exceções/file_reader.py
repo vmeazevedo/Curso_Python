112f3e05da16296741226ed960bd6dd4d3f42a38
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-12 12:43:42 -0300

    Update file_reader.py
    #Lendo dados linha a linha

--- Arquivo_Exceções/file_reader.py
+++ Arquivo_Exceções/file_reader.py
@@ -5,7 +5,22 @@ with open(os.path.join(os.path.dirname(__file__), 'pi_digits.txt')) as file_obje
     contents = file_object.read()
     print(contents.rstrip())
 
+
 #Importando um arquivo por diretorio absoluto
-# with open('C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_digits.txt') as file_object:
-#     contents = file_object.read()
-#     print(contents)
+print()
+with open('C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents)
+
+file_path = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_digits.txt'
+with open(file_path) as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+#Lendo dados linha a linha
+filename = 'C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+#Criando uma lista de linhas de um arquivo
