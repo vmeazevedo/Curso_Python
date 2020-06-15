f222d1eb6bf4e8aa1e6c334e639fafc157ba6c20
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-15 12:28:32 -0300

    programas criados pela função

--- Arquivo_Exceções/write_message.py
+++ Arquivo_Exceções/write_message.py
@@ -7,7 +7,11 @@
 # r+ - modo de leitura e escrita
 
 filename = "C:\\Users\\pqcir\\Documents\\MeusProjetos\\Curso_Python\\Arquivo_Exceções\\programming.txt"
-
 with open(filename, 'w') as file_object:
-    file_object.write('I love programming.')
+    file_object.write('I love programming.')            #A função write cria um arquivo casos ele não existe, porém, caso ele já exista ela irá substitiuir o existente
+
+#Escrevendo em arquivo ser especificar o diretorio absoluto.
+filename = "Arquivo_Exceções\\programming2.txt"         #Para definir diretório sem ser absoluto é so colocar uma pasta do diretorio atual separado por duas \\
+with open(filename, 'w') as file_object:
+    file_object.write('I love programming2.')  
 
