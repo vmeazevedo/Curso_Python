2b0b8ad4e54ff018f6489b41da7126c6f1e10c48
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-15 13:40:13 -0300

    Update alice.py
    contando as palavras de um arquivo

--- Arquivo_Exceções/alice.py
+++ Arquivo_Exceções/alice.py
@@ -6,3 +6,18 @@ try:
 except FileNotFoundError:
     msg = 'Sorry, the file ' + filename + ' does not exist.'
     print(msg)
+
+#Analisando textos
+filename = 'Arquivo_Exceções\\alice.txt'
+try:
+    with open(filename) as file_object:
+        contents = file_object.read()
+except FileNotFoundError:
+    msg = 'Sorry, the file ' + filename + ' does not exist.'
+    print(msg)
+else:
+    #Conta o número aproximado de palavras no arquivo.
+    words = contents.split()
+    num_words = len(words)
+    print('The file ' + filename + ' has about ' + str(num_words) + ' words.')
+
