7902c512b61d151bcf652d939a0eca6074147e2e
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-15 12:38:02 -0300

    update

--- Arquivo_Exceções/write_message.py
+++ Arquivo_Exceções/write_message.py
@@ -28,5 +28,13 @@ with open(filename, 'w') as file_object:
 
 #======================================================
 
-
+#Concatenando dados em um arquivo
+filename = 'Arquivo_Exceções\\programming_concat.txt'
+with open(filename, 'w') as file_object:
+    file_object.write('I love programming.\n')       
+    file_object.write('I love creating new games.\n')
+    
+with open(filename, 'a') as file_object:
+    file_object.write('I also love finding meaning in large datasets. \n')
+    file_object.write('I love creating apps that can run in a browser. \n')
 
