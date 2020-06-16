25e7c97793c43606c3f57ea4f52b191bffd93679
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-16 12:04:24 -0300

    Update words_county.py
    falhando silenciosamente

--- Arquivo_Exceções/words_county.py
+++ Arquivo_Exceções/words_county.py
@@ -16,3 +16,23 @@ filenames = 'Arquivo_Exceções\\alice.txt', 'Arquivo_Exceções\\siddharta.txt'
 for n in filenames:
     count_words(n)
 
+
+#Falhando silenciosamente
+print()
+def count_words(filename):
+    '''Conta o número aproximado de palavras em um arquivo.'''
+    try:
+        with open(filename) as file_object:
+            contents = file_object.read()
+    except FileNotFoundError:
+        pass
+    else:
+        '''Conta o número aproximado de palavras no arquivo.'''
+        words = contents.split()
+        num_words = len(words)
+        print('The file ' + filename + ' has about ' + str(num_words) + ' words.')
+
+filenames = 'Arquivo_Exceções\\alice.txt', 'Arquivo_Exceções\\siddharta.txt', 'Arquivo_Exceções\\moby_dick.txt', 'Arquivo_Exceções\\litte_women.txt'
+for n in filenames:
+    count_words(n)
+    
