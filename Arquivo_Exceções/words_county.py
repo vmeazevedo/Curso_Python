b4d982724f612b313e50c27a604a98867af22b19
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-16 12:45:40 -0300

    criando novos txt

--- Arquivo_Exceções/words_county.py
+++ Arquivo_Exceções/words_county.py
@@ -103,3 +103,7 @@ while True:
         break
 
 '''
+10.8 - Gatos e cachorros: Crie dois arquivos, cats.txt e dogs.txt. Armazene pelo menos três nomes de gatos no primeiro arquivo e três nomes
+de cachorro no segundo arquivo. Escreva um programa que tente ler esses arquivos e mostre o conteudo do arquivo na tela. Coloque seu código
+em um bloco try-except para capturar o erro FileNotFound e apresente uma mensagem simpatica caso o arquivo nao esteja presente. Mova um dos 
+arquivos para um local diferente de seu sistema e garante que o codigo no bloco except seja executado de forma apropriada.'''
