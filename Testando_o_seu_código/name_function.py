40ab6de90127aeb7b6c2ef989bbfaaf4899c625d
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-17 13:34:20 -0300

    Update name_function.py
    add uma função que corrige o erro do teste unitário

--- Testando_o_seu_código/name_function.py
+++ Testando_o_seu_código/name_function.py
@@ -1,10 +1,20 @@
-#Testando uma função
+#Testando uma função - Essa função inicialmente passa em nosso teste unitário
 # def get_formatted_name(first, last):
 #     '''Gera um nome completo formatado de modo elegante.'''
 #     full_name = first + ' ' + last
 #     return full_name.title()
 
-def get_formatted_name(first, middle, last):
+#Testando uma função - Essa função inicialmente não passará em nosso teste unitário pois falta 1 argumento.
+# def get_formatted_name(first, middle, last):
+#     '''Gera um nome completo formatado de modo elegante.'''
+#     full_name = first + ' ' + middle + ' ' + last
+#     return full_name.title()
+
+#Testando uma função - Essa função sempre passará em nosso teste unitário
+def get_formatted_name(first, last, midle=''):
     '''Gera um nome completo formatado de modo elegante.'''
-    full_name = first + ' ' + middle + ' ' + last
+    if midle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
