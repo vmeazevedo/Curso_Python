43cfc9d0de8b146e7ee2f150cc6b15c04cf6aa0d
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-17 13:28:03 -0300

    Update name_function.py
    realizando um teste que falha

--- Testando_o_seu_código/name_function.py
+++ Testando_o_seu_código/name_function.py
@@ -1,6 +1,10 @@
 #Testando uma função
-def get_formatted_name(first, last):
-    '''Gera um nome completo formatado de modo elegante.'''
-    full_name = first + ' ' + last
-    return full_name.title()
+# def get_formatted_name(first, last):
+#     '''Gera um nome completo formatado de modo elegante.'''
+#     full_name = first + ' ' + last
+#     return full_name.title()
 
+def get_formatted_name(first, middle, last):
+    '''Gera um nome completo formatado de modo elegante.'''
+    full_name = first + ' ' + middle + ' ' + last
+    return full_name.title()
