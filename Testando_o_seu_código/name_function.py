9689319e0356c47908f3aafc2847fb6dce393cea
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-06-17 13:25:25 -0300

    Create name_function.py

---
+++ Testando_o_seu_código/name_function.py
@@ -0,0 +1,6 @@
+#Testando uma função
+def get_formatted_name(first, last):
+    '''Gera um nome completo formatado de modo elegante.'''
+    full_name = first + ' ' + last
+    return full_name.title()
+
