12837a7c6363efe7313de7979d04188d90a00b69
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-07-05 13:19:05 -0300

    Update scatter_squares.py
    update

--- Gerando Dados/scatter_squares.py
+++ Gerando Dados/scatter_squares.py
@@ -1,102 +1,102 @@
 #Plotando e estilizando pontos individuais com scatter()
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# plt.scatter(2,4, s=200)                                     #S é o tamanho do ponto a ser apresentado
+plt.scatter(2,4, s=200)                                     #S é o tamanho do ponto a ser apresentado
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o tamanho dos rótulos das marcações'''
-# plt.tick_params(axis='both', which='major', labelsize=14)
-# plt.show()
+'''Define o tamanho dos rótulos das marcações'''
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
 
 #Plotando uma série de pontos com scatter()
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# x_values = [1,2,3,4,5]                                      #Dados de entrada
-# y_values = [1,4,9,16,25]                                    #Dados de saída
-# plt.scatter(x_values, y_values, s=100)                      #S é o tamanho do ponto a ser apresentado
+x_values = [1,2,3,4,5]                                      #Dados de entrada
+y_values = [1,4,9,16,25]                                    #Dados de saída
+plt.scatter(x_values, y_values, s=100)                      #S é o tamanho do ponto a ser apresentado
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o tamanho dos rótulos das marcações'''
-# plt.tick_params(axis='both', which='major', labelsize=14)
-# plt.show()
+'''Define o tamanho dos rótulos das marcações'''
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
 
 #Calculando dados automaticamente
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
-# y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
+x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
+y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
 
-# plt.scatter(x_values, y_values, s=40)                   #Passamos os valores de x e y para o scatter
+plt.scatter(x_values, y_values, s=40)                   #Passamos os valores de x e y para o scatter
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o intervalo para cada eixo'''
-# plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
-# plt.show()
+'''Define o intervalo para cada eixo'''
+plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
+plt.show()
 
 #Definindo cores personalizadas
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
-# y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
+x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
+y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
 
-# # plt.scatter(x_values, y_values, c='red', s=40)                   #Passando c no scatter podemos selecionar outra cor para a linha
-# plt.scatter(x_values, y_values, c=(0, 1, 0), s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
+# plt.scatter(x_values, y_values, c='red', s=40)                   #Passando c no scatter podemos selecionar outra cor para a linha
+plt.scatter(x_values, y_values, c=(0, 1, 0), s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o intervalo para cada eixo'''
-# plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
-# plt.show()
+'''Define o intervalo para cada eixo'''
+plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
+plt.show()
 
 
 #Usando um colormap
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
-# y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
+x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
+y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
 
-# plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o intervalo para cada eixo'''
-# plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
-# plt.show()
+'''Define o intervalo para cada eixo'''
+plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
+plt.show()
 
 #Salvando seus gráficos automaticamente
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
-# y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
+x_values = list(range(1,1001))                          #Criamos uma lista de valores de x de 1 a 1000
+y_values = [x**2 for x in x_values]                     #Analisamos os valores de x da lista x_values e elevamos ele ao quadrados
 
-# plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=40)                 #Passando c no scatter podemos selecionar outra cor para a linha
 
-# '''Define o título do gráfico e nomeia os eixos'''
-# plt.title('Square Numbers', fontsize=24)
-# plt.xlabel('Value', fontsize=14)
-# plt.ylabel('Square of Value', fontsize=14)
+'''Define o título do gráfico e nomeia os eixos'''
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
 
-# '''Define o intervalo para cada eixo'''
-# plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
-# plt.savefig('Gerando Dados\\squares_plot.png', bbox_inches='tight')     #Salva a imagem do gráfico no diretório que estamos trabalhando.
+'''Define o intervalo para cada eixo'''
+plt.axis([0, 1100, 0, 1100000])                 #Especificamos o intervalo de cada eixo, a função axis exige 4 valores: os valores min e max para cada eixo.
+plt.savefig('Gerando Dados\\squares_plot.png', bbox_inches='tight')     #Salva a imagem do gráfico no diretório que estamos trabalhando.
 
 
 
@@ -108,6 +108,7 @@ import matplotlib.pyplot as plt
 x_values = [1,2,3,4,5]
 y_values = [1,8,27,64,125]
 
+#Gráfico 1
 plt.scatter(x_values, y_values, s=40)
 plt.title('Square Numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)
@@ -115,6 +116,7 @@ plt.ylabel('Square of Value', fontsize=14)
 plt.tick_params(axis='both', labelsize=14)
 plt.show()
 
+#Gráfico 2
 x_values = list(range(1,5001))
 y_values = [x**3 for x in x_values]
 plt.scatter(x_values, y_values, s=40)
